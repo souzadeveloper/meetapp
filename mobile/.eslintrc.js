43e30a0578829024d45c02312357c84f03ad0152
Marcelo <souzadeveloper@gmail.com>
2019-10-13 11:52:31 -0300

    Reacriando projeto

--- mobile/.eslintrc.js
+++ mobile/.eslintrc.js
@@ -3,16 +3,22 @@ module.exports = {
     es6: true,
   },
   extends: [
-    'airbnb-base',
+    'airbnb',
   ],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
   },
   parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
     ecmaVersion: 2018,
     sourceType: 'module',
   },
+  plugins: [
+    'react',
+  ],
   rules: {
   },
 };
