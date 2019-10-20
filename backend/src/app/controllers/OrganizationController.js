1c62b025b28e2d74df953b53535b742abdf714d0
Marcelo <souzadeveloper@gmail.com>
2019-10-20 00:41:47 -0300

    Ordenação por Data

--- backend/src/app/controllers/OrganizationController.js
+++ backend/src/app/controllers/OrganizationController.js
@@ -2,7 +2,10 @@ import Meetup from '../models/Meetup';
 
 class OrganizationController {
   async index(req, res) {
-    const meetups = await Meetup.findAll({ where: { user_id: req.userId } });
+    const meetups = await Meetup.findAll({
+      where: { user_id: req.userId },
+      order: [['date']],
+    });
 
     return res.json(meetups);
   }
